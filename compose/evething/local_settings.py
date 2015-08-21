0a20d4ef637952a0d3246baca42bf9bb0045cb1b
Rob Haswell <me@robhaswell.co.uk>
2015-08-21 12:00:29 +0100

    Steal some things from the DIST docker settings.

--- compose/evething/local_settings.py
+++ compose/evething/local_settings.py
@@ -41,6 +41,21 @@ DATABASES = {
         'PORT': '',
     },
 }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+        },
+    },
+}
 
 EMAIL_HOST = b"mail_1"
 
